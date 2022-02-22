d024dfa7a8e94bd348d26cb07e145e624c189d45
Dragos Neata <dragos.neatax@gmail.com>
2022-02-22 12:38:36 +0200

    urls

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('account.urls', namespace='account'))
+    path('account/', include('account.urls', namespace='account')),
+    path('', views.display_data, name='display_data'),
 ]
